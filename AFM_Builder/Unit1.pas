73e7ac9f1f0f92f8ec03fbfc6d5cbb7e40013667
W. Latif Ayubi <wayubi@gmail.com>
2003-12-25 01:21:00 +0000

    Updated AFM format. Old AFM was bad.
    git-svn-id: svn+ssh://svn.code.sf.net/p/fusion/code/tools/trunk@2373 a2e91628-aa1e-412e-9966-34abe3d8d708

--- AFM_Builder/Unit1.pas
+++ AFM_Builder/Unit1.pas
@@ -11,9 +11,6 @@ type TBlock = class
 	Mob         :TIntList32;
 	CList       :TIntList32;
 	MobProcTick :cardinal;
-
-	//constructor Create;
-	//destructor Destroy; override;
 end;
 
 type TMap = class
@@ -35,7 +32,7 @@ end;
 
 type TMapList = class
 	Name      :string;
-        Ext       :string;
+    Ext       :string;
 	Size      :TPoint;
 	Mode      :byte;
 end;
@@ -60,109 +57,117 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
-	str     :string;
-        dat     :TFileStream;
-        dat2     :TMemoryStream;
-	tm      :TMap;
-	h  :array[0..3] of single;
-	maptype :integer;
+	    str         :string;
+        dat         :TMemoryStream;
+	    tm          :TMap;
+	    h           :array[0..3] of single;
+	    maptype     :integer;
 
-        sr	:TSearchRec;
-        w       :word;
-        xy      :TPoint;
-	ta	:TMapList;
+        sr	        :TSearchRec;
+        w           :word;
+        xy          :TPoint;
+	    ta	        :TMapList;
 
-        i,j     :integer;
+        i,j         :integer;
 
-        mapcoord :array[0..600] of array[0..600] of byte;
+        mapcoord    :array[0..600] of array[0..600] of byte;
 
-        FWM :TextFile;
-        OUT :TextFile;
+        FWM         :TextFile;
+        OUT         :TextFile;
 
-        zipfile :TZip;
-        path    :string;
-        animals : TStringList;
-        dir     :string;
+        zipfile     :TZip;
+        path        :string;
+        animals     :TStringList;
+        dir         :string;
+
+        AppPath     :string;
 begin
 
-        tm := nil;
-        MapList := nil;
+    MapList := nil;
+    AppPath := GetCurrentDir;
 
-        if FindFirst('map\*.gat', $27, sr) = 0 then begin
+    if FindFirst('map\*.gat', $27, sr) = 0 then begin
 		repeat
-			dat := TFileStream.Create('map\' + sr.Name, fmOpenRead, fmShareDenyWrite);
-
-			SetLength(str, 4);
-                        dat.Read(str[1], 4);
-                        if str <> 'GRAT' then begin
-                                MessageBox(Handle, PChar('Map Format Error : ' + sr.Name), 'eWeiss', MB_OK or MB_ICONSTOP);
-                                Application.Terminate;
-                                exit;
-			end;
+            dat := TMemoryStream.Create;
+            dat.LoadFromFile(AppPath + '\map\' + sr.Name);
+            SetLength(str, 4);
+            dat.Read(str[1], 4);
 
 			dat.Read(w, 2);
 			dat.Read(xy.X, 4);
 			dat.Read(xy.Y, 4);
 
-			ta := TMapList.Create;
-			ta.Name := LowerCase(ChangeFileExt(sr.Name, ''));
-                        ta.Ext := 'gat';
-			ta.Size := xy;
-			ta.Mode := 0;
-			//MapList.AddObject(ta.Name, ta);
+            str  := StringReplace(sr.Name, '.gat', '',[rfReplaceAll, rfIgnoreCase]);
 
-                        str  := StringReplace(sr.Name, '.gat', '',[rfReplaceAll, rfIgnoreCase]);
+            //SetLength(mapcoord, xy.X, xy.Y);
 
-                        AssignFile(FWM, 'afm/'+str+'.out');
-                        ReWrite(FWM);
+            for j := 0 to xy.Y - 1 do begin
+                for i := 0 to xy.X - 1 do begin
 
-                        WriteLn(FWM, 'ADVANCED FUSION MAP');
-                        WriteLn(FWM, str);
-                        WriteLn(FWM, inttostr(xy.X)+' '+inttostr(xy.Y));
-                        WriteLn(FWM);
+                    dat.Read(h[0], 4);
+                    dat.Read(h[1], 4);
+                    dat.Read(h[2], 4);
+                    dat.Read(h[3], 4);
+                    dat.Read(maptype, 4);
 
-                        if checkbox1.Checked then AssignFile(OUT, 'afm/debug-'+str+'.txt');
-                        if checkbox1.Checked then ReWrite(OUT);
-
-                        if checkbox1.Checked then WriteLn(OUT, str+' '+inttostr(xy.X)+' '+inttostr(xy.Y));
-
-                       	for j := 0 to xy.Y - 1 do begin
-                		for i := 0 to xy.X - 1 do begin
-                                        if ta.Ext = 'gat' then begin
-                                                dat.Read(h[0], 4);
-                		        	dat.Read(h[1], 4);
-		                	        dat.Read(h[2], 4);
-        			                dat.Read(h[3], 4);
-                	        		dat.Read(maptype, 4);
-
-                                                if (maptype = 0) then begin
-                                                        if (h[0] > 3) or (h[1] > 3) or (h[2] > 3) or (h[3] > 3) then begin
-                        					mapcoord[i][j] := 3;
-                                                        end else begin
-                		        			mapcoord[i][j] := 1;
-                                                        end;
-                                                end else if (maptype = 5) then begin
-                                                        mapcoord[i][j] := 0;
-                                                end;
-
-                                        end;
-
-                                        // WRITE OUT
-
-                                        Write(FWM, mapcoord[i][j]);
-                                        if checkbox1.Checked then WriteLn(OUT, '('+inttostr(i)+','+inttostr(j)+') ['+inttostr(mapcoord[i][j])+']');
-
-                                        // WRITE OUT
-
-
-                                end;
-                                WriteLn(FWM);
+                    if (maptype = 0) then begin
+                        if (h[0] > 3) or (h[1] > 3) or (h[2] > 3) or (h[3] > 3) then begin
+                            mapcoord[i][j] := 3;
+                        end else begin
+                            mapcoord[i][j] := 1;
                         end;
+                    end else if (maptype = 5) then begin
+                        mapcoord[i][j] := 0;
+                    end;
 
-                        CloseFile(FWM);
-                        if checkbox1.Checked then CloseFile(OUT);
-                        label1.Caption := 'Conversion Complete';
-                        dat.Free;
+                end;
+            end;
+
+            dat.Free;
+
+            AssignFile(FWM, 'afm/'+str+'.afm');
+            ReWrite(FWM);
+            WriteLn(FWM, 'ADVANCED FUSION MAP');
+            WriteLn(FWM, str);
+            WriteLn(FWM, inttostr(xy.X)+' '+inttostr(xy.Y));
+            WriteLn(FWM);
+
+            if checkbox1.Checked then begin
+                AssignFile(OUT, 'afm/debug-'+str+'.txt');
+                ReWrite(OUT);
+                WriteLn(OUT, str+' '+inttostr(xy.X)+' '+inttostr(xy.Y));
+            end;
+
+            for j := 0 to xy.Y - 1 do begin
+                for i := 0 to xy.X - 1 do begin
+
+                    Write(FWM, mapcoord[i][j]);
+
+                    if checkbox1.Checked then begin
+                        WriteLn(OUT, '('+inttostr(i)+','+inttostr(j)+') ['+inttostr(mapcoord[i][j])+']');
+                    end;
+
+                end;
+                WriteLn(FWM);
+            end;
+
+            CloseFile(FWM);
+            if checkbox1.Checked then CloseFile(OUT);
+
+            label1.Caption := 'Conversion Complete';
+
+        until FindNext(sr) <> 0;
+		FindClose(sr);
+    end;
+
+    tm.Free;
+
+end;
+
+end.
+
+
+{
 
                         dir := GetCurrentDir;
 
@@ -183,10 +188,9 @@ begin
 
                         animals.Free;
 
-		until FindNext(sr) <> 0;
-		FindClose(sr);
+
 
 	end;
 end;
 
-end.
+}
